0316e8e94e98267508fe355ddd9ae4001c47aed1
EkaterinaSukhanova <katrinsuh@yandex.ru>
2018-07-11 20:34:20 +0300

    lesson 4 with var 2

--- lesson_4/lesson_4.js
+++ lesson_4/lesson_4.js
@@ -12,3 +12,13 @@ function foo (a) {
 }
 
 console.log(foo(5)(6)());
+
+let result = 1;
+function foo2 (arg) {
+    if (arg === undefined) {
+        return result;
+    }
+    result = result * arg;
+    return foo2;
+}
+console.log(foo2(4)(6)());
