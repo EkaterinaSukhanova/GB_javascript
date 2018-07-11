fb912bde2bf0b6c913c9da9c4904ba3a84d7ad44
EkaterinaSukhanova <katrinsuh@yandex.ru>
2018-07-11 20:02:28 +0300

    lesson 3

--- lesson_3/lesson_3.js
+++ lesson_3/lesson_3.js
@@ -1,5 +1,7 @@
-for (let i = 1e5 + 3; i <= 15e4; i++) {
-    if ((3 * i - 6) % 100 == 52 && String(i)[1] == 2) {
-        console.log(i);
+for(let n = 10000; n <= 150000; n++){
+    if (String(n)[1] === '2'){
+        if (((n - 1) + (n - 2) + (n - 3)) % 100 === 52) {
+            console.log(n);
+        }
     }
 }
