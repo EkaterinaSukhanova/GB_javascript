9067e98d89c5f5257380c1a353a131e98e67250d
EkaterinaSukhanova <katrinsuh@yandex.ru>
2018-07-16 20:57:46 +0300

    lesson 7

--- lesson_7/lesson_7.js
+++ lesson_7/lesson_7.js
@@ -1,30 +1,67 @@
 'use strict';
 
+const genres = ["Детектив", "Комедия", "Триллер"];
+const films = [];
 
-class Birds {
-    constructor(name) {
+class Film {
+    constructor(name, numGenre, year = 0, country = ""){
         this.name = name;
+        this.genre = genres[numGenre];
+        this.year = year;
+        this.country = country;
+        this.comments = [];
     }
-
-    checkProperty(bird) {
-        console.log("in check");
+    addComment(text, author, numberStars){
+        this.comments.push( new Comment(text, author, numberStars) );
     }
-
-    static checkPropertyStatic() {
-        console.log("in check static");
+    rating(){
+        let sumStars = 0;
+        this.comments.forEach( comment => sumStars += comment.numberStars);
+        return (this.comments.length > 0) ? sumStars / this.comments.length : 0;
     }
-
 }
-//
-// let ob = {
-//   value1: 234,
-//   value2: 456
-// };
-//
-// if (ob.hasOwnProperty("value1")) {
-//     console.log(ob.value1);
-// }
-//
-// console.log(Obconstject.keys(ob));
 
-Birds.checkPropertyStatic();
+class Comment{
+    constructor(text, userAuthor, numberStars){
+        this.text = text;
+        this.author = userAuthor;
+        this.numberStars = numberStars;
+    }
+}
+
+films.push( new Film("Поцелуй навылет", 1, 2005, "США"));
+films[0].addComment("Очень смешно! Всем советую.", "Alina94", 5);
+films[0].addComment("Осторожно, черный юмор.", "Nico222", 3);
+
+films.push( new Film("В джазе только девушки", 1, 1959, "США"));
+films[1].addComment("Милый и забавный фильм.", "Niko222", 4);
+films[1].addComment("Хороший фильм.", "Alina94", 5);
+films[1].addComment("Хороший фильм для вечернего отдыха. Очень понравился.", "Moti87Put", 5);
+
+films.push( new Film("Невидимый гость", 0, 2016, "Испания"));
+films[2].addComment("Смотрел на одном дыхании! Очень интересно.", "Moti87Put", 5);
+films[2].addComment("Странный конец. Не для меня.", "Niko222", 3);
+
+films.push( new Film("Исходный код", 2, 2011, "США, Канада"));
+films[3].addComment("Не стал моим любимым фильмом, но он хорош.", "Niko222", 4);
+
+films.push( new Film("Престиж", 2, 2006, "США, Великобритания"));
+films[4].addComment("Кто любит фильмы, в которых все становится понятно " +
+    "только в конце, это для вас! Рекомендую.", "Moti87Put", 5);
+
+function getFilmsGenre( genre ) {
+    const foundFilms = [];
+    for (let film of films){
+        if (film.genre === genre){
+            foundFilms.push(film);
+        }
+    }
+    return foundFilms;
+}
+
+
+console.log(films[2].rating());
+
+console.log(getFilmsGenre("Комедия"));
+
+
