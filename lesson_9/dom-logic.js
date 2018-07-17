4f6a94619651a3b4df6c81646ab277680074e23a
EkaterinaSukhanova <katrinsuh@yandex.ru>
2018-07-17 23:57:32 +0300

    lesson 9 киноучет

---
+++ lesson_9/dom-logic.js
@@ -0,0 +1,112 @@
+const openFilm = {};
+
+document.addEventListener("DOMContentLoaded", function () {
+    for (let genre of genres){
+        const newEL = document.createElement("div");
+        newEL.classList.add("genre");
+        newEL.innerText = genre;
+        newEL.addEventListener("click", function() {
+            onGenreChoice(genre);
+        });
+        document.querySelector(".genres").appendChild(newEL);
+    }
+});
+
+function getFilmByName(filmName) {
+    return films.filter( f => f.name === filmName)[0];
+}
+
+function getFilmComments(filmName) {
+    const film = getFilmByName(filmName);
+    return film.comments;
+
+}
+
+function onAddCommentClick(name) {
+    const authorValue = document.getElementById(`author-${name}`).value;
+    const commentValue = document.getElementById(`comment-${name}`).value;
+    const film = getFilmByName(name);
+    film.addComment(commentValue, authorValue);
+    onGenreChoice(film.genre)
+}
+
+function renderCommentForm(film) {
+    const content = `<div class="form-title">Добавьте отзыв фильму ${film.name}</div>
+        <div class="form-body">
+        <input id="author-${film.name}" class="form-author" placeholder="Ваше имя">
+        <input id="comment-${film.name}" class="form-comment" placeholder="Ваш отзыв">
+        <button onclick="onAddCommentClick('${film.name}')">Отправить</button>
+        </div>`;
+    const form = document.createElement("div");
+    form.classList.add("comment-form");
+    form.innerHTML = content;
+
+    form.addEventListener("click", function (event) {
+       event.stopPropagation();
+       form.classList.add("chosen");
+    });
+    return form;
+}
+
+function openFilmCard(film, newEl) {
+    const comments = getFilmComments(film.name);
+    let s = "";
+    comments.forEach( c => {
+        s += `<div class="comment"><span class="comment-author">${c.author}</span>: ${c.text}</div>`;
+    });
+    newEl.innerHTML +=
+        `<div class="film-comment">${s}</div>`;
+
+    const addCommentButton = document.createElement("button");
+    addCommentButton.innerText = "Добавить отзыв";
+    addCommentButton.addEventListener("click", function (event) {
+        event.stopPropagation();
+        const commentForm = renderCommentForm(film);
+        newEl.appendChild(commentForm);
+        newEl.removeChild(addCommentButton);
+    });
+    newEl.appendChild(addCommentButton);
+
+}
+
+function onFilmClick(film, newEl) {
+    if(openFilm.hasOwnProperty(film.name)
+        && openFilm[film.name] === true){
+        newEl.innerHTML =
+            `<img src='img/${film.img}' alt='${film.name}'>
+             <div class="film-name">
+                   <p>${film.name}</p>
+                   <p>Год: ${film.year}</p>
+                   <p>Страна: ${film.country}</p></div>
+             <div class="clearfix"></div>`;
+        openFilm[film.name] = false;
+    } else {
+        openFilmCard(film, newEl);
+        openFilm[film.name] = true;
+    }
+}
+
+function renderFilm(film) {
+    const newEl = document.createElement("div");
+    newEl.classList.add("film");
+    newEl.innerHTML = `<img src='img/${film.img}' alt='${film.name}'>
+                       <div class="film-name">
+                            <p>${film.name}</p>
+                            <p>Год: ${film.year}</p>
+                            <p>Страна: ${film.country}</p></div>
+                        <div class="clearfix"></div>`;
+
+    newEl.addEventListener("click", function () {
+        onFilmClick(film, newEl);
+    });
+
+    document.querySelector(".films").appendChild(newEl);
+}
+
+function onGenreChoice(genreName) {
+    document.querySelector(".films").innerHTML = "";
+    const films = getFilmsGenre(genreName);
+    for (let film of films){
+        renderFilm(film);
+    }
+}
---
+++ lesson_9/script.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const genres = ["Детектив", "Комедия", "Триллер"];
+const films = [];
+
+class Film {
+    constructor(name, numGenre, year = 0, country = "", imageName){
+        this.name = name;
+        this.genre = genres[numGenre];
+        this.year = year;
+        this.country = country;
+        this.comments = [];
+        this.img = imageName + ".jpg";
+    }
+    addComment(text, author, numberStars){
+        this.comments.push( new Comment(text, author, numberStars) );
+    }
+    rating(){
+        let sumStars = 0;
+        this.comments.forEach( comment => sumStars += comment.numberStars);
+        return (this.comments.length > 0) ? sumStars / this.comments.length : 0;
+    }
+}
+
+class Comment{
+    constructor(text, userAuthor, numberStars){
+        this.text = text;
+        this.author = userAuthor;
+        this.numberStars = numberStars;
+    }
+}
+
+films.push( new Film("Поцелуй навылет", 1, 2005, "США", "kiss_kiss"));
+films[0].addComment("Очень смешно! Всем советую.", "Alina94", 5);
+films[0].addComment("Осторожно, черный юмор.", "Nico222", 3);
+
+films.push( new Film("В джазе только девушки", 1, 1959, "США", "jazz"));
+films[1].addComment("Милый и забавный фильм.", "Niko222", 4);
+films[1].addComment("Хороший фильм.", "Alina94", 5);
+films[1].addComment("Хороший фильм для вечернего отдыха. Очень понравился.", "Moti87Put", 5);
+
+films.push( new Film("Невидимый гость", 0, 2016, "Испания", "guest"));
+films[2].addComment("Смотрел на одном дыхании! Очень интересно.", "Moti87Put", 5);
+films[2].addComment("Странный конец. Не для меня.", "Niko222", 3);
+
+films.push( new Film("Исходный код", 2, 2011, "США, Канада", "code"));
+films[3].addComment("Не стал моим любимым фильмом, но он хорош.", "Niko222", 4);
+
+films.push( new Film("Престиж", 2, 2006, "США, Великобритания", "prestige"));
+films[4].addComment("Кто любит фильмы, в которых все становится понятно " +
+    "только в конце, это для вас! Рекомендую.", "Moti87Put", 5);
+
+function getFilmsGenre( genre ) {
+    const foundFilms = [];
+    for (let film of films){
+        if (film.genre === genre){
+            foundFilms.push(film);
+        }
+    }
+    return foundFilms;
+}
+
+
